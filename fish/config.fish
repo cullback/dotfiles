0aa3f6533a47effc7e1160c7ad8d5908f6e852b2
cullback <cullback@fastmail.com>
2025-04-22 11:36:26 -0400

    add secret reading

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,20 @@
 #!/usr/bin/env fish
 
+function get_secret -a key
+    keepassxc-cli show -a password "$HOME/secrets.kdbx" \
+        --key-file "$HOME/keepass.keyx" \
+        --no-password "$key"
+    # keychain way
+    # security find-generic-password -a "$USER" -s "$key" -w
+end
+
+set -gx OPENROUTER_API_KEY (get_secret "OPENROUTER_API_KEY")
+
 if status is-interactive
     set fish_greeting # Suppress fish welcome message
 
     fish_config theme choose catppuccin-frappe
+    fish_config prompt choose Arrow
 
     set -gx EDITOR hx
 
