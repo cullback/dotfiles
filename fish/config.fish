146a3deef7a2161001a21295f84f7e9970b22f97
cullback <cullback@fastmail.com>
2025-04-29 22:36:05 -0400

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,26 @@
 #!/usr/bin/env fish
 
-function get_secret -a key
-    keepassxc-cli show -a password "$HOME/secrets.kdbx" \
-        --key-file "$HOME/keepass.keyx" \
-        --no-password "$key"
-    # keychain way
-    # security find-generic-password -a "$USER" -s "$key" -w
-end
+# function get_secret -a key
+#     keepassxc-cli show -a password "$HOME/secrets.kdbx" \
+#         --key-file "$HOME/keepass.keyx" \
+#         --no-password "$key"
+#     # keychain way
+#     # security find-generic-password -a "$USER" -s "$key" -w
+# end
 
-set -gx OPENROUTER_API_KEY (get_secret "OPENROUTER_API_KEY")
+# set -gx OPENROUTER_API_KEY (get_secret "OPENROUTER_API_KEY")
+
+fish_add_path --move /opt/homebrew/bin
+fish_add_path --move $HOME/.local/bin/
+
+set -gx SHELL (which fish)
+set -gx EDITOR hx
 
 if status is-interactive
     set fish_greeting # Suppress fish welcome message
 
     fish_config theme choose catppuccin-frappe
     fish_config prompt choose Arrow
-
-    set -gx EDITOR hx
 
     set -gx FZF_DEFAULT_COMMAND "fd --type f"
     set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
