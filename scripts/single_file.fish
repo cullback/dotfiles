4bce3259bb8606981cd0d4041a5fab33002ea0a5
cullback <cullback@fastmail.com>
2025-08-02 22:04:08 -0400

    update single-file script

--- scripts/single_file.fish
+++ scripts/single_file.fish
@@ -3,4 +3,4 @@
 # https://github.com/gildas-lormeau/SingleFile/blob/f550b1daf07efa86169c732dc4dc6f710d783e77/src/ui/pages/help.html#L26
 # "template variables"
 
-single-file --filename-template "{url-hostname} {date-iso} {page-title}.{filename-extension}" $argv[1]
+single-file --filename-template "{url-hostname} - {date-iso} - {page-title}.{filename-extension}" $argv[1]
