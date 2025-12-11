25a9b40d52df60c310e572b01c79f897ffa76128
cullback <cullback@fastmail.com>
2025-12-11 16:39:14 -0500

    Add syncthing folders and restore facter.json
    Configure syncthing to manage admin and notes folders with device
    overrides. Restore facter.json to version control as required by
    nix flakes.

--- hosts/common/syncthing.nix
+++ hosts/common/syncthing.nix
@@ -8,10 +8,33 @@
 
     guiAddress = "0.0.0.0:8384";
 
-    overrideDevices = false;
-    overrideFolders = false;
+    overrideDevices = true;
+    overrideFolders = true;
 
     settings = {
+      devices = {
+        iphone14 = {
+          id = "P7D6TDJ-EM4PIG6-W3AHLYZ-VVSQVME-7AOS5E3-7FPAPCM-52GAQZO-XAVKCQ7";
+        };
+        macbook-air = {
+          id = "O2QNTQH-2XGCZ6N-7TP7QXA-E22L6IG-J3EPPTQ-R7LMVSX-KKSPSSI-FKSHJAB";
+        };
+      };
+      folders = {
+        "admin" = {
+          path = "/home/cullback/vault/admin";
+          devices = [
+            "iphone14"
+            "macbook-air"
+          ];
+        };
+        "notes" = {
+          path = "/home/cullback/vault/repos/notes";
+          devices = [
+            "iphone14"
+          ];
+        };
+      };
       gui = {
         insecureSkipHostcheck = true;
         insecureAdminAccess = true;
